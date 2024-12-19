4f6cbd1f24a632fd53a126fceb510686fcbcf6cf
unknown <Bgoodluck>
2024-12-19 20:06:20 +0100

    vercel

--- src/helpers/uploadImage.jsx
+++ src/helpers/uploadImage.jsx
@@ -1,33 +1,3 @@
-// const uploadImage = async (image) => {
-//     const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/image/upload`;
-
-//     try {
-//         const formData = new FormData();
-//         formData.append('file', image);
-//         formData.append('upload_preset', process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET);
-
-//         const response = await fetch(url, {
-//             method: 'POST',
-//             body: formData
-//         });
-
-//         if (!response.ok) {
-//             const errorData = await response.json().catch(() => null);
-//             console.error('Upload error details:', errorData);
-//             throw new Error(`Upload failed: ${response.status} ${response.statusText}`);
-//         }
-
-//         const data = await response.json();
-//         return data;
-//     } catch (error) {
-//         console.error('Error uploading image:', error);
-//         throw error;
-//     }
-// };
-
-// export default uploadImage;
-
-
 const uploadImage = async (image) => {
     const cloudName = "dmefsl3py";
     const uploadPreset = "jobportfolio";
